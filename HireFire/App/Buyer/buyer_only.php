95e477e4b1a1d8fcf48ddc7e7b32f03dcfda45fc
TANIM <iktanim1995@gmail.com>
2017-12-26 02:15:07 +0600

    image format check

--- HireFire/App/Buyer/buyer_only.php
+++ HireFire/App/Buyer/buyer_only.php
@@ -25,25 +25,45 @@
 <?php
 	//var_dump($GLOBALS);
 	if($_SERVER['REQUEST_METHOD']=="POST"){
-		$name=$_FILES['file']['name'];//ByDefault Name of the uplpoaded file
+		$filename=$_FILES['file']['name'];//ByDefault Name of the uplpoaded file
 		$tmp_name=$_FILES['file']['tmp_name'];//ByDefault where the image saved
-		//$size=$_FILES['file']['size'];
-		//===
-		if(isset($name))
+		$size=$_FILES['file']['size'];
+		if(isset($filename))
 		{
-			$name="TANIM.jpg";
-			if(!empty($name))
-			{
-				//echo $userName;
-				$location="../uploads/";
-				if(move_uploaded_file($tmp_name,$location.$username.".jpg")){
-					$imageLocationWithImageName="../uploads/".$username.".jpg";
-					echo "Uploaded";
+			if($size<5000000){
+				if($filename!=""){
+					$fileExt=explode(".",$filename);
+					if(count($fileExt>=2)){
+						if(($fileExt[1]=='jpg')||($fileExt[1]=='png')||($fileExt[1]=='jpeg')){
+							$location="../uploads/";
+							if(move_uploaded_file($tmp_name,$location.$username.".jpg")){
+								$imageLocationWithImageName="../uploads/".$username.".jpg";
+								echo "<script>alert('Uploaded')</script>";
+							}
+							
+						}
+						else{
+							echo "<script>alert('Please check your image format')</script>";
+						}
+					}
+					//var_dump($fileExt);
 				}
 			}
-		}else{
-			echo "Please choose a file";
+			else{
+				echo "<script>alert('Your File size is too large')</script>";
+			}
+			
 		}
+		//===
+		/*if(isset($filename))
+		{
+			//$name="TANIM.jpg";
+			if(!empty($filename))
+			{
+				//echo $userName;
+				
+			}
+		}*/
 		
 	}
 ?>
