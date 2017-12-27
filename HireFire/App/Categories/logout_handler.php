4aaeee12b3e78f2c572add434ea2002d86a4de12
reza <rezaul.bd77@gmail.com>
2017-12-28 01:06:17 +0600

    order progress database

---
+++ HireFire/App/Categories/logout_handler.php
@@ -0,0 +1,12 @@
+<?php   session_start(); 
+		$path=__DIR__."/../data/person_data_access(reza).php";
+	include($path); 
+?>
+
+<?php 
+	     $username = $_SESSION['username'];
+		 //echo "<script>document.location='Orders_pending.php'</script>";
+		 activetimeToDb($username);
+		 unset($_SESSION);
+		 echo "<script>document.location='PublicHome.html'</script>";
+?>
