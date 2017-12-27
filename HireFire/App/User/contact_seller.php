1594d241de9256b5cc18dfb5fc19647b02eb75b1
Robiullah <Robiullah2244@gmail.com>
2017-12-27 23:23:04 +0600

    contact_seller page complete

--- HireFire/App/User/contact_seller.php
+++ HireFire/App/User/contact_seller.php
@@ -2,16 +2,44 @@
 	session_start();
 ?>
 <?php 
-	require_once "../../data/person_data_access(reza).php";
+	require_once "../../service/person_service(robi).php";
 		require_once "../../service/validation_service(tanim).php";
 ?>
 <?php
 ?>
 
 <?php 
-     $username = $_REQUEST['username'];
-	$buyer=accessProfileBuyer($username);
-	  $month=monthReturn($buyer[0]['joiningDate']);
+     $gigId = $_REQUEST['gigId'];
+	 
+	$allGig=accessUser($gigId);
+	//var_dump($allGig);
+	for($i=0; $row = mysqli_fetch_assoc($allGig); ++$i)
+	{
+		
+		//echo "<script>alert('Programming2')</script>";
+		//$programmingAndTechGig[$i] = $row;
+		$sellerName=$row['uName'];
+	}
+	
+	$sellerDetails=accessSellerDetails($sellerName);
+	for($i=0; $row = mysqli_fetch_assoc($sellerDetails); ++$i)
+	{
+		
+		//echo "<script>alert('Programming2')</script>";
+		$programmingAndTechGig[$i] = $row;
+		$sellerName=$programmingAndTechGig[$i]['uName'];
+		$joiningDate=$programmingAndTechGig[$i]['joiningDate'];
+		$expLevel=$programmingAndTechGig[$i]['expertLevel'];
+		$country=$programmingAndTechGig[$i]['country'];
+		$workingHour=$programmingAndTechGig[$i]['workingHour'];
+		
+	}	
+	
+	//$allGig=accessGig($gigId);
+	
+		
+		
+	  /*$month=monthReturn($buyer[0]['joiningDate']);
 	 $persons=accessProfileSeller($username);
 	 var_dump($persons);
 	 $day=getJoiningDateFromDb($username);
@@ -20,7 +48,7 @@
 	 
 	 $skills=(getSkillsBySellerFromDb($username));
 	 
-	 
+	 */
 	 //var_dump($languages[1]['language']);
 ?>
 
@@ -53,7 +81,7 @@
 		<td width="1%"></td>
 		<td valign="top" align="center" width="20%">
 			<img src="../image/b.png" width="30%" alt="TANIM"/>
-			<br/>Ibrahim Khalil<hr/>
+			<br/><?php echo $sellerName; ?><hr/>
 			
 			<table width="100%">
 			
@@ -66,26 +94,26 @@
 			<tr>
 				<td width="5%"><img src="../image/location.png"/></td>
 				<td width="60%">Form</td>
-				<td align="right">Bangladesh</td>
+				<td align="right"><?php echo $country; ?></td>
 			</tr>
 			<tr>
 				<td width="5%"><img src="../image/member1.png"/></td>
 				<td>Member since</td>
-				<td align="right">April 2017</td>
+				<td align="right"><?php echo $joiningDate; ?></td>
 			</tr>
 			
 			<tr>
 				<td width="5%"><img src="../image/time.png"/></td>
-				<td>Average Response Time</td>
-				<td align="right">1Hrs.</td>
+				<td><hr/>Working Hour Per Week</td>
+				<td align="right"><?php echo $workingHour; ?></td>
 			</tr>
 			
 			<tr height="10">
 				<td colspan="3"><hr/></td>
 			</tr>
 			<tr>
-				<td colspan="2"><font size="4"><b>Languages</font></b>
-				<br/>English</td>
+				<td colspan="2"><font size="4"><b>Experience Level : </b></font>
+				<td align="right"><b><?php echo $expLevel; ?></b></td>
 				
 			</tr>
 			<tr height="10">
@@ -109,10 +137,38 @@
 		<h1>Active Gigs	</h1><br/><br/>
 		<table cellspacing="40">
 		<tr>
-			<td width="20%">	
-				<img src="../image/project1.jpg" width="30%"/><br/><br/>
-				<a href="../gig/details.html">I will do c or c++ project for you.</a>
-			</td>
+		<?php
+			$count=0;
+			$allGig=accessAllGigs($sellerName);
+			//var_dump($allGig);
+			$gigShow=array();
+			for($i=0; $row = mysqli_fetch_assoc($allGig); ++$i)
+			{
+			
+				$gigShow[$i]=$row;
+				//var_dump($gigShow[$i]);
+				if($i%4==0){
+					echo "<tr>";
+				}
+				$imgName=$gigShow[$i]['imgExt'];
+				//$gigId=$imgName;
+				$gigTitle=$gigShow[$i]['gigTitle'];		
+				
+				echo "<td width='20%'>
+					<img src='../GigImage/$imgName' width='200' hight='200'/><br/><br/>
+					<a href='../gig/proceedToBuy.php?gigId=".$gigId."'>$gigTitle</a>
+				</td>";
+				$count++;
+				if($count==4)
+				{
+					echo "</tr>";
+					$count=0;
+					//$count=3;
+				}
+				
+				
+			}
+		?>
 		</tr>
 		</table>
 		</td>
