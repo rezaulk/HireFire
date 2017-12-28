86380fed5ceb3b2cd6a2a1f78e5704256cfeac97
TANIM <iktanim1995@gmail.com>
2017-12-29 02:00:17 +0600

    company profit entry added

--- HireFire/App/User/Order_file_uploaded.php
+++ HireFire/App/User/Order_file_uploaded.php
@@ -7,7 +7,21 @@
     $pass="";
     $dbname="hirefire_db";
     $port=3306;
-   
+	function checkIftheGigAlreadyExistInTheProfitList($gId){
+		global $host, $user, $pass, $dbname, $port;
+		$conn=mysqli_connect($host, $user, $pass, $dbname, $port);
+		$sql="select * from companyprofit where gId=$gId";
+		var_dump($sql);
+		$result = mysqli_query($conn, $sql);
+		$j=0;
+		for($i=0; $row = mysqli_fetch_assoc($result); ++$i){
+			$j++;
+		}
+		
+		mysqli_close($conn);
+		return $j; 
+		
+	}
    function addGigFile($person)
 	{
 		global $host, $user, $pass, $dbname, $port;
@@ -22,17 +36,68 @@
 		mysqli_close($conn);
 		return($result);
 	}
+	function insertprofitToDB($gid,$numberOforder,$price){
+		$totalProfit=$numberOforder*$price*.20;
+		global $host, $user, $pass, $dbname, $port;
+		$conn=mysqli_connect($host, $user, $pass, $dbname, $port);
+		 $sql = "INSERT INTO companyprofit (gId, profit) VALUES ($gid,$totalProfit)";
+		$result = mysqli_query($conn, $sql);
+		mysqli_close($conn);		
+		return($result);
+		
+	}
+	
+	function updateprofitToDB($gid,$numberOforder,$price){
+		global $host, $user, $pass, $dbname, $port;
+		$conn=mysqli_connect($host, $user, $pass, $dbname, $port);
+			$totalProfit=$numberOforder*$price*.20;
+		$sql="UPDATE companyprofit SET profit='$totalProfit' WHERE gId=$gid";
+		//var_dump($sql);
+		$result = mysqli_query($conn, $sql);
+		mysqli_close($conn);
+		return($result);
+		
+		
+	}
+	
 	 function retrieveGigPrice($person)
 	{
 		global $host, $user, $pass, $dbname, $port;
 		$conn=mysqli_connect($host, $user, $pass, $dbname, $port);
-		$gigid=$person["gId"];
-		$sql="select price from gigs where gigId=$gigid";
-		//var_dump($sql);
+		$gigid=$person["gigId"];
+		var_dump($gigid);
+		$sql="select * from gigs where gigId=$gigid";
+		var_dump($sql);
+		$persons=array();
 		$result = mysqli_query($conn, $sql);
-		$row=mysqli_fetch_assoc($result);
+		
+		for($i=0; $row = mysqli_fetch_assoc($result); ++$i){
+			$persons[$i] = $row;
+		}
+		
 		mysqli_close($conn);
-		return($row);
+		return $persons; 
+		
+		//return($row);
+	}
+	function retrieveOrderNumber($person)
+	{
+		global $host, $user, $pass, $dbname, $port;
+		$conn=mysqli_connect($host, $user, $pass, $dbname, $port);
+		$sName=$person["sName"];
+		$bName=$person["bName"];
+		$gigid=$person["gId"];
+		$fileExt=$person['fileExt'];
+		$sql="select * from orders WHERE gId=$gigid AND status='complete'";
+		var_dump($sql);
+		$person=array();
+		$result = mysqli_query($conn, $sql);
+		$j=0;
+		for($i=0; $row = mysqli_fetch_assoc($result); ++$i){
+			$j++;
+		}var_dump($i);
+		mysqli_close($conn);
+		return($j);
 	}
    
   
@@ -47,10 +112,11 @@
 		
 	}
 	if(!empty($buyerName))
-   {
+	{
 		$_SESSION['bName']=$buyerName;
 		
 	}
+	
 	$gId=$_SESSION['gId'];
 	$bName=trim($_SESSION['bName']);
 	$sName=$_SESSION['username'];
@@ -73,14 +139,30 @@
 							$x=addGigFile($person);
 							//retrive gig price
 							
-							
-							$price=retrieveGigPrice(2);
+							$gig['gigId']=$gId;
+							$price=retrieveGigPrice($gig);
 							var_dump($price);
+							$money=$price[0]['price'];
+							$order=retrieveOrderNumber($person);
+							var_dump($order);
+							if($order>0){
+								if(checkIftheGigAlreadyExistInTheProfitList($gId)==0){
+									if(insertprofitToDB($gId,$order,$money)){
+										echo "Insert";
+									}
+									
+								}
+								else{
+									updateprofitToDB($gId,$order,$money);
+									echo "Update";
+								}
+								
+							}
 							
 							//var_dump($x);
 							//ob_end_flush();
 							//echo "<script>alert('Uploaded')</script>";
-							echo "<script>alert('Uploaded');document.location='Orders_active.php'</script>";
+							//echo "<script>alert('Uploaded');document.location='Orders_active.php'</script>";
 						}	
 					}
 					else{
